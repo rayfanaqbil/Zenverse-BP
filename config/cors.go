c1898af2f2bdd16387df2d3728afa36f1e27d845
rayfanaqbil <rayfana09@gmail.com>
2024-06-27 12:40:10 +0700

    modif cors

--- config/cors.go
+++ config/cors.go
@@ -1,31 +1,31 @@
 package config
 
 import (
-	"os"
-	"strings"
+    "os"
+    "strings"
 
-	"github.com/gofiber/fiber/v2/middleware/cors"
+    "github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 var origins = []string{
-	"https://auth.ulbi.ac.id",
-	"https://sip.ulbi.ac.id",
-	"https://euis.ulbi.ac.id",
-	"https://home.ulbi.ac.id",
-	"https://alpha.ulbi.ac.id",
-	"https://dias.ulbi.ac.id",
-	"https://iteung.ulbi.ac.id",
-	"https://whatsauth.github.io",
-	"https://rayfanaqbil.github.io",
-	"http://127.0.0.1:5500/",
-	"https://hrisz.github.io",
+    "https://auth.ulbi.ac.id",
+    "https://sip.ulbi.ac.id",
+    "https://euis.ulbi.ac.id",
+    "https://home.ulbi.ac.id",
+    "https://alpha.ulbi.ac.id",
+    "https://dias.ulbi.ac.id",
+    "https://iteung.ulbi.ac.id",
+    "https://whatsauth.github.io",
+    "https://rayfanaqbil.github.io",
+    "http://127.0.0.1:5500",
+    "https://hrisz.github.io",
 }
 
 var Internalhost string = os.Getenv("INTERNALHOST") + ":" + os.Getenv("PORT")
 
 var Cors = cors.Config{
-	AllowOrigins:     strings.Join(origins[:], ","),
-	AllowHeaders:     "Origin,Login",
-	ExposeHeaders:    "Content-Length",
-	AllowCredentials: true,
+    AllowOrigins:     strings.Join(origins[:], ","),
+    AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+    ExposeHeaders:    "Content-Length",
+    AllowCredentials: true,
 }
