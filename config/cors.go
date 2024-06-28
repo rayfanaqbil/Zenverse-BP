7cd7ca73d356df6f16727227ecc7e8960c140d8d
rayfanaqbil <rayfana09@gmail.com>
2024-06-29 00:09:46 +0700

    modify cors again

--- config/cors.go
+++ config/cors.go
@@ -19,14 +19,13 @@ var origins = []string{
     "https://rayfanaqbil.github.io",
     "http://127.0.0.1:5500",
     "https://hrisz.github.io",
-    "/form",
 }
 
 var Internalhost string = os.Getenv("INTERNALHOST") + ":" + os.Getenv("PORT")
 
 var Cors = cors.Config{
     AllowOrigins:     strings.Join(origins[:], ","),
-    AllowHeaders:     "Origin, Content-Type, Accept, Authorization, Username",
+    AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
     ExposeHeaders:    "Content-Length",
     AllowCredentials: true,
 }
