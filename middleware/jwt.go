6d2d4bbf92cf34a0ce8c896d8fd5cb348eec2678
rayfanaqbil <rayfana09@gmail.com>
2024-07-01 06:52:43 +0700

    modif: middleware

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -1,30 +1,31 @@
 package middleware
 
 import (
-	"github.com/gofiber/fiber/v2"
-	"github.com/dgrijalva/jwt-go"
-	inimodel "github.com/rayfanaqbil/zenverse-BE/model"
+    "github.com/gofiber/fiber/v2"
+    "github.com/dgrijalva/jwt-go"
+    "github.com/rayfanaqbil/zenverse-BE/model"
 )
 
 var jwtKey = []byte("ZnVRsERfnHRsZ")
 
 func AuthMiddleware() fiber.Handler {
-	return func(c *fiber.Ctx) error {
-		tokenStr := c.Get("Authorization")
-		if tokenStr == "" {
-			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Missing token"})
-		}
+    return func(c *fiber.Ctx) error {
+        tokenStr := c.Get("Authorization")
+        if tokenStr == "" {
+            return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Missing token"})
+        }
 
-		tokenStr = tokenStr[len("Bearer "):]
-		claims := &inimodel.Admin{}
-		token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
-			return jwtKey, nil
-		})
-		if err != nil || !token.Valid {
-			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Invalid token"})
-		}
+        tokenStr = tokenStr[len("Bearer "):]
 
-		c.Locals("username", claims.User_name)
-		return c.Next()
-	}
+        claims := &model.Admin{}
+        token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
+            return jwtKey, nil
+        })
+        if err != nil || !token.Valid {
+            return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Invalid token"})
+        }
+
+        c.Locals("username", claims.User_name)
+        return c.Next()
+    }
 }
