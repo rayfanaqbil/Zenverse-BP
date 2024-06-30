bfedc72ac7f1e6e5d5f828ec9bd8212d517fe510
rayfanaqbil <rayfana09@gmail.com>
2024-07-01 05:05:25 +0700

    modif: url

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -3,7 +3,7 @@ package middleware
 import (
     "github.com/gofiber/fiber/v2"
     "github.com/dgrijalva/jwt-go"
-    inimodel "github.com/rayfanaqbil/zenverse-BE/model"
+    "github.com/rayfanaqbil/zenverse-BE/model"
 )
 
 var jwtKey = []byte("ZnVRsERfnHRsZ")
@@ -16,15 +16,14 @@ func AuthMiddleware() fiber.Handler {
         }
 
         tokenStr = tokenStr[len("Bearer "):]
-        claims := &inimodel.Claims{}
+
+        claims := &model.Claims{}
         token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
             return jwtKey, nil
         })
         if err != nil || !token.Valid {
             return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Invalid token"})
         }
-
-
         c.Locals("username", claims.UserName)
         return c.Next()
     }
--- url/url.go
+++ url/url.go
@@ -27,9 +27,9 @@ func Web(page *fiber.App, db *mongo.Database) {
     page.Delete("/delete/:id", controller.DeleteGamesByID)
     page.Get("/docs/*", swagger.HandlerDefault)
     page.Post("/insert", controller.InsertDataGames)
-    page.Post("/login", handler.Login)
 
     page.Use(middleware.AuthMiddleware())
+    page.Post("/login", handler.Login)
     page.Post("/logout", handler.Logout)
     page.Get("/token", controller.GetDataToken)
 }
