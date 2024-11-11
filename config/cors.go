b9abf784aeeebe17e3543bea58b618a73927f5a5
rayfanaqbil <rayfana09@gmail.com>
2024-11-11 22:18:50 +0700

    modify cors

--- config/cors.go
+++ config/cors.go
@@ -1,48 +1,61 @@
-package config
+    package config
 
-import (
-    "os"
-    "strings"
+    import (
+        "os"
+        "strings"
 
-    "github.com/gofiber/fiber/v2"
-    "github.com/gofiber/fiber/v2/middleware/cors"
-)
+        "github.com/gofiber/fiber/v2"
+        "github.com/gofiber/fiber/v2/middleware/cors"
+    )
 
-// Daftar origins yang diizinkan untuk CORS
-var origins = []string{
-    "https://auth.ulbi.ac.id",
-    "https://sip.ulbi.ac.id",
-    "https://euis.ulbi.ac.id",
-    "https://home.ulbi.ac.id",
-    "https://alpha.ulbi.ac.id",
-    "https://dias.ulbi.ac.id",
-    "https://iteung.ulbi.ac.id",
-    "https://whatsauth.github.io",
-    "https://rayfanaqbil.github.io",
-    "http://127.0.0.1:5500",
-    "https://hrisz.github.io",
-}
+    // Daftar origins yang diizinkan untuk CORS
+    var origins = []string{
+        "https://auth.ulbi.ac.id",
+        "https://sip.ulbi.ac.id",
+        "https://euis.ulbi.ac.id",
+        "https://home.ulbi.ac.id",
+        "https://alpha.ulbi.ac.id",
+        "https://dias.ulbi.ac.id",
+        "https://iteung.ulbi.ac.id",
+        "https://whatsauth.github.io",
+        "https://rayfanaqbil.github.io",
+        "http://127.0.0.1:5500",
+        "https://hrisz.github.io",
+    }
 
-// Mendapatkan host internal dari environment variables
-var Internalhost string = os.Getenv("INTERNALHOST") + ":" + os.Getenv("PORT")
+    // Mendapatkan host internal dari environment variables
+    var Internalhost string = os.Getenv("INTERNALHOST") + ":" + os.Getenv("PORT")
 
-// Konfigurasi CORS
-var Cors = cors.Config{
-    AllowOrigins:     strings.Join(origins, ","),
-    AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
-    ExposeHeaders:    "Content-Length",
-    AllowCredentials: true,
-}
+    // Konfigurasi CORS
+    var Cors = cors.Config{
+        AllowOrigins:     strings.Join(origins, ","),
+        AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+        ExposeHeaders:    "Content-Length",
+        AllowCredentials: true,
+    }
 
-// Fungsi untuk setup CORS dan COOP di aplikasi Fiber
-func SetupCorsAndCOOP(app *fiber.App) {
-    // Middleware untuk CORS
-    app.Use(cors.New(Cors))
+    // Fungsi untuk setup CORS dan COOP di aplikasi Fiber
+    func SetupCorsAndCOOP(app *fiber.App) {
+        // Middleware untuk CORS
+        app.Use(cors.New(Cors))
+    
+        // Middleware untuk menambahkan header keamanan
+        app.Use(func(c *fiber.Ctx) error {
+            origin := c.Get("Origin")
+            if contains(origins, origin) {
+                c.Set("Cross-Origin-Opener-Policy", "same-origin")
+                c.Set("Cross-Origin-Embedder-Policy", "require-corp")
+            }
+            return c.Next()
+        })
+    }
 
-    // Middleware untuk menambahkan header Cross-Origin-Opener-Policy dan Cross-Origin-Embedder-Policy
-    app.Use(func(c *fiber.Ctx) error {
-        c.Set("Cross-Origin-Opener-Policy", "same-origin")
-        c.Set("Cross-Origin-Embedder-Policy", "require-corp")
-        return c.Next()
-    })
-}
+    func contains(slice []string, item string) bool {
+        for _, v := range slice {
+            if v == item {
+                return true
+            }
+        }
+        return false
+    }
+    
